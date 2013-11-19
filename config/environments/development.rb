df62e6ed69799555668740e47ad5b66163d6fd21
Joel AZEMAR <joel.azemar@gmail.com>
2013-11-19 11:12:53 +0100

    set ember config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,4 +24,9 @@ TodoMVC::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.ember.variant = :development
+  config.handlebars.precompile = true
+  config.ember.app_name = 'TodoMVC'
+
 end
